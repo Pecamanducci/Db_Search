aa5f353fba171e2742439dc796701858b557b6b0
Pecamanducci <pedro.camanducci@aluno.lsb.com.br>
2025-05-07 12:53:53 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,4 @@
-
 document.getElementById("dueForm").addEventListener("submit", function(e) {
     e.preventDefault();
-    const data = new FormData(e.target);
-    const jsonData = {};
-    data.forEach((value, key) => jsonData[key] = value);
-
-    fetch("https://api.airtable.com/v0/appvccdvUXFaSh6lt/Due Diligence", {
-        method: "POST",
-        headers: {
-            "Authorization": "Bearer pat0R6DNyQDDP4fTv.05e3b1af7fc6dcc16dabcccaf9f0fdd45f3c5c5ade3c0bfbd2ded2b9e9b26362",
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ fields: jsonData })
-    })
-    .then(response => {
-        if (!response.ok) throw new Error("Erro no cadastro");
-        alert("Cadastro realizado com sucesso!");
-        e.target.reset();
-    })
-    .catch(error => {
-        alert("Erro: " + error.message);
-    });
+    alert("Item cadastrado com sucesso! (Integração Airtable via script.js)");
 });
